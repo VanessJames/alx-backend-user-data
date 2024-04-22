6ec0ad6208e692e4f4631131f79ad34bda80ca56
VanessJames <vanessjames10@gmail.com>
2024-04-22 19:50:57 +0000

    user

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -27,18 +27,18 @@ def stats() -> str:
 
 
 @app_views.route('/unauthorized/', strict_slashes=False)
-def unauthorized() -> None:
-    """GET /api/v1/unauthorized
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
     Return:
-      - Unauthorized error.
+      - abort(401)
     """
     abort(401)
 
 
 @app_views.route('/forbidden/', strict_slashes=False)
-def forbidden() -> None:
-    """GET /api/v1/forbidden
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
     Return:
-      - Forbidden error.
+      - abort(403)
     """
     abort(403)
